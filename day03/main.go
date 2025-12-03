2911a1f3553faa53a22f44710a4ef88fb46e1468
IronBeagle404 <190517473+IronBeagle404@users.noreply.github.com>
2025-12-03 13:01:18 +0100

    feat: add day03 part 2

--- day03/main.go
+++ day03/main.go
@@ -45,5 +45,48 @@ func main() {
 		res += totalValue
 	}
 
-	fmt.Printf("RESULT : %v\n", res)
+	partTwoRes := TwelveDigits(banks)
+
+	fmt.Printf("PART 1 RESULT : %v\nPART 2 RESULT : %v\n", res, partTwoRes)
+}
+
+func TwelveDigits(banks []string) int {
+	res := 0
+
+	for _, bank := range banks {
+		joltArr := []int{}
+		for len(joltArr) < 12 {
+			max := 0
+			maxIndex := 0
+
+			for x, char := range bank {
+				charN, _ := strconv.Atoi(string(char))
+				if charN > max {
+					if len(bank)-x < 12-len(joltArr) {
+						break
+					}
+					max = charN
+					maxIndex = x
+				}
+			}
+			joltArr = append(joltArr, max)
+
+			if maxIndex < len(bank)-1 {
+				bank = bank[maxIndex+1:]
+			} else {
+				break
+			}
+		}
+
+		totalStr := ""
+		for _, n := range joltArr {
+			totalStr += strconv.Itoa(n)
+		}
+
+		totalN, _ := strconv.Atoi(totalStr)
+		res += totalN
+
+	}
+
+	return res
 }
